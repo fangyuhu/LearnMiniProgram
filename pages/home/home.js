87835186244da7c8d80d79a9f8643a3b60359ef5
hfy <5529176@163.com>
2020-04-01 19:32:27 +0800

    初始化项目

---
+++ pages/home/home.js
@@ -0,0 +1,29 @@
+// pages/home/home.js
+Page({
+  data:{
+    name:'胡方宇',
+    age:18,
+  }
+})
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
